29436016128c66fb87680f69494e2ecb73905cc4
Mikad02 <caligiacomo0@gmail.com>
2023-12-04 15:05:53 +0100

    punti esclamativi a caso

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("lavorate");
-        System.out.println("i'm the pizzaman");
+        System.out.println("i'm the pizzaman!!!!!!!");
     }
 }
